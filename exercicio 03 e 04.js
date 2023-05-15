a942db1e5360f61dd9cd7f5288cb081e1e41d82e
Thaila Luiza Chiaradia Fagundes da Silva <96634174+ThailaChiaradia@users.noreply.github.com>
2023-05-14 22:19:35 -0300

    Update and rename exercicio 03.js to exercicio 03 e 04.js

---
+++ exercicio 03 e 04.js
@@ -0,0 +1,6 @@
+//3)
+O Print do console é:
+"A função é c"; 
+"A função é d";
+
+//4) O b() tem um return no começo da função então acaba não retornando algo e na função d() está sem o resolve() que retorna o objeto Promise.
--- exercicio 03.js
+++
@@ -1,3 +0,0 @@
-O Print do console é:
-"A função é c"; 
-"A função é d";
