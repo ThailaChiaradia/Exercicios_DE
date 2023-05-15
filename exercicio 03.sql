57edf62ce47e11fbe34b15a56556830b0d8e96a6
Thaila Luiza Chiaradia Fagundes da Silva <96634174+ThailaChiaradia@users.noreply.github.com>
2023-05-14 21:25:37 -0300

    Update exercicio 03.sql

--- exercicio 03.sql
+++ exercicio 03.sql
@@ -1,5 +1,5 @@
-SELECT * FROM Aluno
-UNION
-SELECT * FROM Responsavel
-UNION
-SELECT * FROM Parentesco;
+SELECT * FROM PARENTESCO p
+INNER JOIN Aluno a
+ON p.id_aluno = a.id_aluno
+INNER JOIN Responsavel r
+ON p.id_responsavel = r.id_responsavel;
